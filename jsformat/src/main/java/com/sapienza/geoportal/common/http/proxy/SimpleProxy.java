73823bccc07d1b04c7b9fd6b4667fba8f01f7752
maciejgz <macgzi@gmail.com>
2016-02-02 11:43:59 +0100

    servlety proxy dla ajax (bez sprawdzania certyfikatów)

---
+++ jsformat/src/main/java/com/sapienza/geoportal/common/http/proxy/SimpleProxy.java
@@ -0,0 +1,149 @@
+package com.sapienza.geoportal.common.http.proxy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.security.KeyManagementException;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.Certificate;
+import java.security.cert.X509Certificate;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLPeerUnverifiedException;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "simpleProxyServlet", urlPatterns = { "/simpleproxy" }, initParams = {
+		@WebInitParam(name = "url", value = "http://google.com") })
+public class SimpleProxy extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doAny(req, resp);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doAny(req, resp);
+	}
+
+	private void doAny(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		
+		TrustManager[] trustAllCerts = new TrustManager[] { new X509TrustManager() {
+            public java.security.cert.X509Certificate[] getAcceptedIssuers() {
+                return null;
+            }
+            public void checkClientTrusted(X509Certificate[] certs, String authType) {
+            }
+            public void checkServerTrusted(X509Certificate[] certs, String authType) {
+            }
+        } };
+        // Install the all-trusting trust manager
+        SSLContext sc = null;
+		try {
+			sc = SSLContext.getInstance("SSL");
+		} catch (NoSuchAlgorithmException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        try {
+			sc.init(null, trustAllCerts, new java.security.SecureRandom());
+		} catch (KeyManagementException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+        HttpsURLConnection.setDefaultSSLSocketFactory(sc.getSocketFactory());
+        // Create all-trusting host name verifier
+        HostnameVerifier allHostsValid = new HostnameVerifier() {
+            public boolean verify(String hostname, SSLSession session) {
+                return true;
+            }
+        };
+        
+        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
+
+		
+		String urlString = request.getParameter("url");
+		URL url;
+		InputStream is = null;
+		BufferedReader br = null;
+		String line;
+		PrintWriter writer = null;
+		try {
+
+			url = new URL(urlString);
+			if (urlString.startsWith("https")) {
+				HttpsURLConnection con = (HttpsURLConnection) url.openConnection();
+				print_https_cert(con);
+				is = con.getInputStream();
+			} else {
+				is = url.openStream();
+			}
+
+			// throws an IOException
+			br = new BufferedReader(new InputStreamReader(is));
+			writer = response.getWriter();
+			while ((line = br.readLine()) != null) {
+				writer.println(line);
+			}
+		} catch (MalformedURLException mue) {
+			mue.printStackTrace();
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		} finally {
+			try {
+				if (is != null)
+					is.close();
+			} catch (IOException ioe) {
+				// nothing to see here
+			}
+			if (writer != null) {
+				writer.close();
+			}
+		}
+
+	}
+
+	private void print_https_cert(HttpsURLConnection con) {
+
+		if (con != null) {
+
+			try {
+
+				System.out.println("Response Code : " + con.getResponseCode());
+				System.out.println("Cipher Suite : " + con.getCipherSuite());
+				System.out.println("\n");
+
+				Certificate[] certs = con.getServerCertificates();
+				for (Certificate cert : certs) {
+					System.out.println("Cert Type : " + cert.getType());
+					System.out.println("Cert Hash Code : " + cert.hashCode());
+					System.out.println("Cert Public Key Algorithm : " + cert.getPublicKey().getAlgorithm());
+					System.out.println("Cert Public Key Format : " + cert.getPublicKey().getFormat());
+					System.out.println("\n");
+				}
+
+			} catch (SSLPeerUnverifiedException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+
+	}
+}
--- jsformat/src/main/webapp/scripts/parse.js
+++ jsformat/src/main/webapp/scripts/parse.js
@@ -1,4 +1,36 @@
 $( document ).ready(function() {
- 
+	//download rss feed
+	jQuery.ajax({
+			url : 'http://localhost:8080/jsformat/simpleproxy?url=https://www.disasterscharter.org/charter-portlets/cpi-mvc/activations/feed/rss/',
+			type : 'GET',
+			dataType : "xml",
+			success : loadXsl,
+			error : function(data, status) {
+				alert('error ' + data + ' status ' + status);
+				// write to the console the error message 	
+			}
+		});
  
 });
+
+
+
+function loadXsl(data){
+	console.log("feed: " + data);
+	jQuery.ajax({
+			url : '/jsformat/xls/disasters.xsl',
+			type : 'GET',
+			dataType : "xml",
+			success : function (xml){
+				parseFeed(data,xml);
+			},
+			error : function(data, status) {
+				alert('error ' + data + ' status ' + status);
+				// write to the console the error message 	
+			}
+		});
+}
+
+function parseFeed(feed, formatter){
+	console.log("feed: " + data + "\n\n\n\n\nformatter" + formatter);
+}
